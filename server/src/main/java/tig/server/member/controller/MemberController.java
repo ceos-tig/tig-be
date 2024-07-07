6372032485c8fd8971ed8612c1ed732ced4d2f60
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2024-07-07 15:35:36 +0900

    feat : implement JWT reissue logic

--- server/src/main/java/tig/server/member/controller/MemberController.java
+++ server/src/main/java/tig/server/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import tig.server.annotation.LoginUser;
 import tig.server.error.ApiResponse;
 import tig.server.jwt.TokenProvider;
 import tig.server.member.domain.Member;
@@ -14,13 +15,12 @@ import tig.server.member.service.MemberService;
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
-    private final TokenProvider tokenProvider;
 
     /**
      * refresh token을 통한 access token 재발급
      */
     @PostMapping("/reissue")
-    public ApiResponse<Object> reissueAccessToken(/*@LoginUser*/ Member member,
+    public ApiResponse<Object> reissueAccessToken(@LoginUser Member member,
                                                   @RequestBody RefreshTokenRequestDto refreshTokenRequestDto) {
         String newAccessToken = memberService.reissueAccessToken(member, refreshTokenRequestDto);
         return ApiResponse.builder()
