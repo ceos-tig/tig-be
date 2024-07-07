9d0ad114545e8b79d31b465c4afcd9cc682178d1
yeong-hwan <jghff700@naver.com>
2024-07-08 02:10:05 +0900

    refactor: append dto and mapper for review

---
+++ server/src/main/java/tig/server/review/dto/ReviewDTO.java
@@ -0,0 +1,43 @@
+package tig.server.review.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.OneToOne;
+import lombok.*;
+import tig.server.reservation.domain.Reservation;
+
+public class ReviewDTO {
+
+    @Getter
+    @Setter
+    @ToString
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class Request {
+
+        @Schema(type = "float", example = "4.5")
+        private Integer rating;
+
+        @Schema(type = "string", example = "재미있어요. 티그볼링장 추천합니다.")
+        private String contents;
+
+    }
+
+    @Getter
+    @Setter
+    @ToString
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class Response {
+
+        @Schema(type = "float", example = "4.5")
+        private Integer rating;
+
+        @Schema(type = "string", example = "재미있어요. 티그볼링장 추천합니다.")
+        private String contents;
+
+    }
+
+}
+
