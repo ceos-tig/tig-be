292dd3edd74a74c853db85183f09a4d9854a1af5
yeong-hwan <jghff700@naver.com>
2024-07-12 20:51:25 +0900

    docs: append swagger jwt auth field

--- server/src/main/java/tig/server/config/SwaggerConfig.java
+++ server/src/main/java/tig/server/config/SwaggerConfig.java
@@ -3,23 +3,36 @@ package tig.server.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
         return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(Arrays.asList(securityRequirement));
     }
 
     private Info apiInfo() {
         return new Info()
-                .title("Dears API Documentation")
+                .title("TIG API Documentation")
                 .description("")
                 .version("0.1.0");
     }
+
+
 }
+
+
