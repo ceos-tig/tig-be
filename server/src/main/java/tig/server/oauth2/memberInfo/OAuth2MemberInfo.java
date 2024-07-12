7b7e82c08d33d2f32adce36a9c2dc64483c38b98
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2024-07-12 17:27:14 +0900

    remove : remove existing kakao login logic

--- server/src/main/java/tig/server/oauth2/memberInfo/OAuth2MemberInfo.java
+++
@@ -1,9 +0,0 @@
-package tig.server.oauth2.memberInfo;
-
-public interface OAuth2MemberInfo {
-    String getProviderId();
-    String getProvider();
-    String getName();
-    String getEmail();
-    String getProfileImage();
-}
