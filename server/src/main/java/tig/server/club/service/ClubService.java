c8d0ccb208d977f983a87124fab92931269471d3
yeong-hwan <jghff700@naver.com>
2024-07-24 15:40:41 +0900

    refactor: avg rating round at 1 decimal place

--- server/src/main/java/tig/server/club/service/ClubService.java
+++ server/src/main/java/tig/server/club/service/ClubService.java
@@ -80,6 +80,8 @@ public class ClubService {
     private ClubResponse calculateAvgRating(ClubResponse clubResponse) {
         if (clubResponse.getRatingCount() != 0) {
             float avgRating = (float) clubResponse.getRatingSum() / clubResponse.getRatingCount();
+            // round at 1 decimal place
+            avgRating = Math.round(avgRating * 10) / 10.0f;
             clubResponse.setAvgRating(avgRating);
         }
         return clubResponse;
