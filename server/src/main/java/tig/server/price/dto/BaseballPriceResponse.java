7bd2af488869729225e4e9a37d1bb334946f3507
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2024-10-14 15:29:17 +0900

    refactor : modify response dtos for every sports

--- server/src/main/java/tig/server/price/dto/BaseballPriceResponse.java
+++ server/src/main/java/tig/server/price/dto/BaseballPriceResponse.java
@@ -13,5 +13,5 @@ public class BaseballPriceResponse {
     private ProgramEnum programType;  // 메이저룸, 마이너룸, 몇 회
     private Integer duration;  // 시간 단위 (예: 60분, 30분 등)
     private Integer price;  // 해당 시간에 따른 가격
-//    private Integer hits;  // 타격 회수 (선택적 필드)
+    private Integer inning;  // 타격 회수 (선택적 필드)
 }
--- server/src/main/java/tig/server/price/dto/SquashPriceResponse.java
+++ server/src/main/java/tig/server/price/dto/SquashPriceResponse.java
@@ -13,4 +13,5 @@ public class SquashPriceResponse {
     private ProgramEnum programName;  // 원데이클래스, 스쿼시레슨, 주1회 등
     private Integer durationInMonths;  // 기간 (개월 단위, null 가능)
     private Integer price;  // 해당 프로그램 가격
+    private Integer lessonCount; // 레슨 1회, 2회
 }
