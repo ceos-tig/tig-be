4146e32ad501d8bf5d78918b956f612ae5c0cc87
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2024-11-08 22:12:51 +0900

    fix : modify field name for uniformity

--- server/src/main/java/tig/server/club/dto/CategoryClubResponse.java
+++ server/src/main/java/tig/server/club/dto/CategoryClubResponse.java
@@ -32,7 +32,7 @@ public class CategoryClubResponse {
     private String category;
 
     @Schema(type = "long", example = "1")
-    private Long id;
+    private Long clubId;
 
     @Schema(type = "string", example = "티그 탁구장")
     private String clubName;
