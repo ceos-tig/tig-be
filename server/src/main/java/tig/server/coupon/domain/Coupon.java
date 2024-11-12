66c78914cebb71758c11fa25c7da631858bf0a30
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2024-11-12 18:24:00 +0900

    feat : add soft-delete in coupon

--- server/src/main/java/tig/server/coupon/domain/Coupon.java
+++ server/src/main/java/tig/server/coupon/domain/Coupon.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Where;
 import tig.server.member.domain.Member;
 
 import java.time.LocalDateTime;
@@ -14,6 +15,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Where(clause = "is_deleted = false") // 기본적으로 isDeleted가 false인 데이터만 조회
 public class Coupon {
 
     @Id
@@ -32,4 +34,12 @@ public class Coupon {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
+
+    @Builder.Default
+    private boolean isDeleted = false; // 기본값은 false로 설정하여 삭제되지 않은 것으로 간주
+
+    // Soft delete 메서드
+    public void markAsDeleted() {
+        this.isDeleted = true;
+    }
 }
