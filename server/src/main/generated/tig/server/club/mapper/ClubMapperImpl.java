929c99700a37890ca203f2b6dfa3446874809c6c
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2024-11-15 16:14:02 +0900

    fix : modify access token issue logic

--- server/src/main/generated/tig/server/club/mapper/ClubMapperImpl.java
+++ server/src/main/generated/tig/server/club/mapper/ClubMapperImpl.java
@@ -10,7 +10,7 @@ import tig.server.club.dto.ClubResponse;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-11-09T20:31:42+0900",
+    date = "2024-11-15T12:52:14+0900",
     comments = "version: 1.5.1.Final, compiler: javac, environment: Java 17.0.7 (Oracle Corporation)"
 )
 @Component
