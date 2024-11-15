caba3a9f3682896db1e72b03ed5fda16f3d6b4ff
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2024-11-15 23:07:38 +0900

    fix : add issuing coupon code logic(ADMIN)

---
+++ server/src/main/java/tig/server/coupon/dto/CouponCodeResponseDto.java
@@ -0,0 +1,10 @@
+package tig.server.coupon.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CouponCodeResponseDto {
+    private String code;
+}
