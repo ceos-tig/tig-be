e5e10674f2b2d194d4fc0d96a3ac5ff8d9ebd627
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2024-11-17 16:33:49 +0900

    fix : add @EnableScheduling for scheduler

--- server/src/main/java/tig/server/ServerApplication.java
+++ server/src/main/java/tig/server/ServerApplication.java
@@ -1,21 +1,21 @@
 package tig.server;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.commons.httpclient.HttpClientConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
 @SpringBootApplication
 @EnableFeignClients(basePackages = "tig.server.discord")
 @ImportAutoConfiguration({FeignAutoConfiguration.class, HttpClientConfiguration.class})
 @EnableAspectJAutoProxy
+@EnableScheduling
 public class ServerApplication {
 
 	public static void main(String[] args) {
