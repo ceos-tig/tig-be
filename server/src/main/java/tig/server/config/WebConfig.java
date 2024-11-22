f6851dde4154bcd887eb7d8857edd1d0bdc222ec
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2024-11-23 02:40:20 +0900

    fix : resolve CORS error

--- server/src/main/java/tig/server/config/WebConfig.java
+++ server/src/main/java/tig/server/config/WebConfig.java
@@ -23,10 +23,10 @@ public class WebConfig implements WebMvcConfigurer {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginUserArgumentResolver);
     }
-    @Override
-    public void addCorsMappings(final CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://tigleisure.com")
-                .allowedMethods("GET", "POST","PUT","PATCH","DELETE","OPTIONS");
-    }
+//    @Override
+//    public void addCorsMappings(final CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("https://tigleisure.com")
+//                .allowedMethods("GET", "POST","PUT","PATCH","DELETE","OPTIONS");
+//    }
 }
