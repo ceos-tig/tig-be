e79c7459612b3bd3e32d45c3aea13a945a22828a
JEONG KI MIN <captainkimin@naver.com>
2025-01-17 12:41:18 +0900

    fix: seperate prod/local environments and store AT in Cookie

--- server/src/main/java/tig/server/oauth2/kakao/controller/KakaoController.java
+++ server/src/main/java/tig/server/oauth2/kakao/controller/KakaoController.java
@@ -46,18 +46,17 @@ public class KakaoController {
         LoginMemberResponseDto member = memberService.createKakaoMember(userInfo);
 
 
-        if (origin.equals("https://localhost:3000") || origin.equals("https://localhost:8080")) {
-            // localhost에 대한 쿠키 설정 (두 개의 쿠키 생성)
-            ResponseCookie localCookie = ResponseCookie.from("refreshToken", member.getRefreshToken())
+        if (origin.equals("https://localhost:3000") || origin.equals("https://localhost:8080")) { // 로컬에는 body와 쿠키 모두 전송
+            ResponseCookie accessTokenCookie = ResponseCookie.from("accessToken", member.getAccessToken())
                     .httpOnly(true)
-                    .secure(true) // 로컬 환경에서는 Secure=false
+                    .secure(true)
                     .path("/")
-                    .domain("localhost") // localhost 도메인
-                    .maxAge(14 * 24 * 60 * 60) // 2주 (초 단위)
+                    .domain(".tigleisure.com") // .tigleisure.com 도메인
+                    .maxAge(60 * 60) // 2주 (초 단위)
                     .sameSite("None") // SameSite 설정
                     .build();
 
-            ResponseCookie tigDomainCookie = ResponseCookie.from("refreshToken", member.getRefreshToken())
+            ResponseCookie refreshTokenCookie = ResponseCookie.from("refreshToken", member.getRefreshToken())
                     .httpOnly(true)
                     .secure(true)
                     .path("/")
@@ -67,10 +66,23 @@ public class KakaoController {
                     .build();
 
             // 응답 헤더에 두 개의 쿠키 추가
-            response.addHeader(HttpHeaders.SET_COOKIE, localCookie.toString());
-            response.addHeader(HttpHeaders.SET_COOKIE, tigDomainCookie.toString());
+            response.addHeader(HttpHeaders.SET_COOKIE, accessTokenCookie.toString());
+            response.addHeader(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString());
+
+            LoginAccessTokenResponseDto loginAccessTokenResponseDto = LoginAccessTokenResponseDto.fromMember(member.getAccessToken());
+            ApiResponse<LoginAccessTokenResponseDto> result = ApiResponse.of(200, "Login Success(AccessToken IN RESPONSE)", loginAccessTokenResponseDto);
+
+            return ResponseEntity.ok(result);
         } else if (origin.equals("https://tigleisure.com")) {
-            // 배포 환경에 대한 쿠키 설정
+            ResponseCookie accessTokenCookie = ResponseCookie.from("accessToken", member.getAccessToken())
+                    .httpOnly(true)
+                    .secure(true)
+                    .path("/")
+                    .domain(".tigleisure.com") // .tigleisure.com 도메인
+                    .maxAge(60 * 60)// 2주 (초 단위)
+                    .sameSite("None") // SameSite 설정
+                    .build();
+
             ResponseCookie refreshTokenCookie = ResponseCookie.from("refreshToken", member.getRefreshToken())
                     .httpOnly(true)
                     .secure(true) // HTTPS 환경
@@ -81,12 +93,13 @@ public class KakaoController {
                     .build();
 
             // 응답 헤더에 쿠키 추가
+            response.addHeader(HttpHeaders.SET_COOKIE, accessTokenCookie.toString());
             response.addHeader(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString());
+
+            ApiResponse<LoginAccessTokenResponseDto> result = ApiResponse.of(200, "Login Success(NO AccessToken IN RESPONSE)", null);
+            return ResponseEntity.ok(result);
         }
-
-        LoginAccessTokenResponseDto loginAccessTokenResponseDto = LoginAccessTokenResponseDto.fromMember(member.getAccessToken());
-        ApiResponse<LoginAccessTokenResponseDto> result = ApiResponse.of(200, "Login Success", loginAccessTokenResponseDto);
-
+        ApiResponse<LoginAccessTokenResponseDto> result = ApiResponse.of(200, "LOGIN ERROR", null);
         return ResponseEntity.ok(result);
     }
 }
